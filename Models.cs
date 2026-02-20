a33c2527bba4b06cf169986fdffd0d0d32ad7cd0
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2026-02-19 20:33:11 -0500

    inicio

---
+++ Models.cs
@@ -0,0 +1,12 @@
+﻿namespace AZ204CosmosDBDemo
+{
+    internal class Models
+    {
+        public record Employee(
+            string id,
+            string name,
+            string role,
+            string departament
+        );
+    }
+}
