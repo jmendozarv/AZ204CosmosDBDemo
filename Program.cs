0f4420eb1b8e6e09658d095c7f91b0c8d04604f3
jmendozarv <52211870+jmendozarv@users.noreply.github.com>
2026-02-19 20:34:12 -0500

    inicio

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Azure.Cosmos;
+using static AZ204CosmosDBDemo.Models;
+
+CosmosClient cosmosClient = new CosmosClient(
+    connectionString : "AccountEndpoint=;AccountKey=;"
+    );
+
+string databaseName = "WizLabsDBMend";
+string containerName = "WizLabsContainerMend";
+
+//Get Database
+Database database = cosmosClient.GetDatabase(databaseName);
+Console.WriteLine($"Database Id: {database.Id}");
+
+//Get Container
+Container container = database.GetContainer(containerName);
+Console.WriteLine($"Container Id: {container.Id}");
+
+Employee createItemEmployee = new Employee(
+    id: Guid.NewGuid().ToString(),
+    name: "John Doe",
+    role: "Software Engineer",
+    departament: "IT"
+);
+
+/*
+ItemResponse<Employee> createItemResponse = await container.CreateItemAsync<Employee>(
+    item: createItemEmployee,
+    partitionKey: new PartitionKey("IT"));
+
+Console.WriteLine(createItemResponse.Resource);*/
+
+//REPLACE ITEM
+
+Employee updateItemEmployee = new Employee(
+    id: "4832bce0-09d9-4d8a-adb6-17072ae904b8",
+    name: "John Doe",
+    role: "Backend Java",
+    departament: "IT"
+);
+
+ItemResponse<Employee> replaceItemResponse =  await container.ReplaceItemAsync<Employee>(
+    id: "4832bce0-09d9-4d8a-adb6-17072ae904b8",
+    item: updateItemEmployee,
+    partitionKey: new PartitionKey("IT")
+);
+
+Console.WriteLine(replaceItemResponse.Resource);
